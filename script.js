37f1a98a6efc86a23f7a512010b220df82a8bc04
Robert Vinluan <robert.vinluan@gmail.com>
2016-03-12 15:50:12 -0500

    drums

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-var synth = new Tone.PolySynth(4, Tone.SimpleSynth).toMaster();
+var playing = false;
+
+var pads = new Tone.PolySynth(4, Tone.SimpleSynth).toMaster();
+
+//drums
+var kick = new Tone.DrumSynth().toMaster();
+var snare = new Tone.NoiseSynth().toMaster();
 
 Tone.Transport.timeSignature = 3;
 Tone.Transport.loop = true;
 Tone.Transport.loopEnd = "4m";
-Tone.Transport.start();
 
-fourMeasureComp(undefined, undefined, 0);
-
-function fourMeasureComp(key, progression) {
+function scheduleBasicBeat() {
   for(var i = 0; i < 4; i++) {
     Tone.Transport.schedule(function (time) {
-      synth.triggerAttackRelease("C4", "8n", time);
-      synth.triggerAttackRelease("E4", "8n", "+4n");
-      synth.triggerAttackRelease("G4", "8n", "+4n");
-      synth.triggerAttackRelease("E4", "8n", "+2n");
-      synth.triggerAttackRelease("G4", "8n", "+2n");
+      kick.triggerAttackRelease("C2", "8n", time);
+      snare.triggerAttackRelease("4n", "+4n");
+      snare.triggerAttackRelease("4n", "+4n + 4n");
     }, i + "m");
   }
 }
+
+function scheduleFourMeasureComp(key, progression) {
+  for(var i = 0; i < 4; i++) {
+    let chord = teoria.chord(progression[i]),
+      chordNotes = chord.simple();
+    Tone.Transport.schedule(function (time) {
+      
+      for(var j = 0; j < chordNotes.length; j++) {
+        pads.triggerAttackRelease(chordNotes[j] + "4", "2n");
+      }
+
+    }, i + "m");
+  }
+}
+
+scheduleBasicBeat();
+scheduleFourMeasureComp("C", ["Cmaj7", "Dm7", "Am7", "G7"]);
+
+document.body.addEventListener("keydown", function (e) {
+  if(e.which === 32) {
+    if(playing) {
+      Tone.Transport.pause();
+      playing = false;
+    } else {
+      Tone.Transport.start();
+      playing = true;
+    }
+  }
+})
+
+
